e1ba7bcdddeb1778de8122e678efa3b7f96e584e
mauricio-jmmendes <mauricio.jmmendes@gmail.com>
2014-09-24 01:02:35 -0300

    Add opção de remover uma config de conector da lista

--- src/main/java/com/nodemanager/managedbean/util/Converter.java
+++ src/main/java/com/nodemanager/managedbean/util/Converter.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class Converter {
 
   private String stringConectorList;
@@ -90,6 +95,21 @@ public class Converter {
    */
   public void setStringNameUpOrDownList(String stringNameUpOrDownList) {
     this.stringNameUpOrDownList = stringNameUpOrDownList;
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+  }
+
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
   }
 
 }
